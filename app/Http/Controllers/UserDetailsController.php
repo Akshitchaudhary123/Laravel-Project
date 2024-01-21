2771ee27bf7811aeca8116ac5ca4a0352b174baf
Arjun <dahiyaarjun343@gmail.com>
2024-01-21 17:13:48 +0530

    first_commit

---
+++ app/Http/Controllers/UserDetailsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserDetailsController extends Controller
+{
+    public function test(){
+        return 'arjun  dahiya';
+    }
+}
