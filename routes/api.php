8562f39bb341f62eb37dd5887a8699c3925f5120
dahiyaarjun <108714643+dahiyaarjun@users.noreply.github.com>
2024-03-16 19:31:41 +0530

    fix

--- routes/api.php
+++ routes/api.php
@@ -23,8 +23,8 @@ Route::get('/test',[UserController::class,'test']);
 Route::post('/login',[UserController::class,'login']);
 Route::post('/password-reset',[PasswordResetController::class,'passwordReset']);
 Route::post('/reset/{token}',[PasswordResetController::class,'ResetPassword']);
-Route::post('/reset/by-otp',[PasswordResetController::class,'ResetPasswordByOtp']);
-Route::post('/otp',[PasswordResetController::class,'resetPasswordByOtp']);
+Route::post('/reset/by-otp',[PasswordResetController::class,'resetPasswordByOtp']);
+Route::post('/otp',[PasswordResetController::class,'getOtp']);
 // private routes
 Route::middleware('auth:sanctum')->group(function(){
     Route::post('/logout',[UserController::class,'logout']);
